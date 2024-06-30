6b00c1f01ee118fc62344000bf6b167d676ba638
Stefan Guggisberg <stefanguggisberg76@gmail.com>
2024-06-30 10:18:46 +0200

    header files

---
+++ header/info.md
@@ -0,0 +1,3 @@
+# To compile using GCC
+
+gcc -o my_app main.c foo.c
--- header/main.c
+++ header/main.c
@@ -1,7 +1,7 @@
 #include "foo.h"
 int main()
 {
-     int y = foo(3);
+    int y = foo(3);
     printf("%d\n", y);
     return 0;
 }
