8ebe336066f091bed0d60bdbf061d68e0d860919
Stefan Guggisberg <stefanguggisberg76@gmail.com>
2025-05-28 09:52:21 +0200

    doc and array added

---
+++ array/array.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    int my_matrix2[3][2] = {
+        {1, 2},
+        {3, 4},
+        {5, 6},
+    };
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            std::cout << my_matrix2[i][j] << '\n';
+        }
+    }
+
+    return 0;
+}
