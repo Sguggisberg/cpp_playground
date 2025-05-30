3f5794d08724a15880977fcbe383b9103e911bea
Stefan Guggisberg <stefanguggisberg76@gmail.com>
2025-05-30 17:37:11 +0200

    struct

---
+++ struct/struct.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+enum Role
+{
+    ADMIN,
+    USER
+};
+
+struct Test
+{
+    std::string name;
+    std::uint32_t id;
+    Role role;
+};
+
+void print_info(Test &test)
+{
+    switch (test.role)
+    {
+    case Role::ADMIN:
+        std::cout << "Hallo Admin\n";
+        break;
+    case Role::USER:
+        std::cout << "Hallo User\n";
+        break;
+    default:
+        break;
+    }
+}
+
+int main()
+{
+    Test test = {"Stefan", 123, Role::USER};
+    print_info(test);
+    return 0;
+}
